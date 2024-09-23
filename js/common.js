d5e5c8358fdfd007c311bdcf2683bed1e40d6b1c
rabbihossen514 <rabbihossen347@gmail.com>
2024-09-23 23:00:18 +0600

    added few function on javascript

--- js/common.js
+++ js/common.js
@@ -1,5 +1,11 @@
-function addMoneyInputValue(id){
-    const textValue = document.getElementById(id).innerText;
-    const textNumber = textValue;
-    return textNumber;
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputValueNumber = parseFloat(inputValue);
+    return inputValueNumber;
+}
+
+function getInputFieldInnerTextById(id){
+    const inputValue = document.getElementById(id).innerText;
+    const inputValueNumber = parseFloat(inputValue);
+    return inputValueNumber;
 }
--- js/home.js
+++ js/home.js
@@ -1,10 +1,54 @@
-// document.getElementById('donation-money')
-//     .addEventListener('click', function(){
-        
-//     })
-
-
 document.getElementById('add-money-btn-1')
-    .addEventListener('click', function(){
-        console.LOG('INPUT added')
+    .addEventListener('click', function () {
+        const mainBalance = getInputFieldInnerTextById('main-balance');
+        const donateMoney = getInputFieldInnerTextById('donate-money-1');
+        const addMoney = getInputFieldValueById('add-money-input-1');
+        if (addMoney > 0) {
+            const balance = mainBalance - addMoney;
+            document.getElementById('main-balance').innerText = balance;
+            const donate = donateMoney + addMoney;
+            document.getElementById('donate-money-1').innerText = donate;
+            alert('Congratulations')
+        }
+        else {
+            alert('invalid input.')
+        }
     })
+
+document.getElementById('add-money-btn-2')
+    .addEventListener('click', function () {
+        const mainBalance = getInputFieldInnerTextById('main-balance');
+        const donateMoney = getInputFieldInnerTextById('donate-money-2');
+        const addMoney = getInputFieldValueById('add-money-input-2');
+        if (addMoney > 0) {
+            const balance = mainBalance - addMoney;
+            document.getElementById('main-balance').innerText = balance;
+            const donate = donateMoney + addMoney;
+            document.getElementById('donate-money-2').innerText = donate;
+            alert('Congratulations')
+        }
+        else {
+            alert('invalid input.')
+        }
+    })
+
+document.getElementById('add-money-btn-3')
+    .addEventListener('click', function () {
+        const mainBalance = getInputFieldInnerTextById('main-balance');
+        const donateMoney = getInputFieldInnerTextById('donate-money-3');
+        const addMoney = getInputFieldValueById('add-money-input-3');
+        if (addMoney > 0) {
+            const balance = mainBalance - addMoney;
+            document.getElementById('main-balance').innerText = balance;
+            const donate = donateMoney + addMoney;
+            document.getElementById('donate-money-3').innerText = donate;
+            alert('Congratulations')
+        }
+        else {
+            alert('invalid input.')
+        }
+    })
+// document.getElementById('add-money-btn-2')
+//     .addEventListener('click', function () {
+//         const addMoney2 =
+//     })
